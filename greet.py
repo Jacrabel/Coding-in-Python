ab2ee83472fd95c69f9095f0ff8fad54d62d65b1
Jacrabel <scorpiosmw@gmail.com>
2021-06-10 20:31:10 +0800

    Upload greet

---
+++ greet.py
@@ -0,0 +1,15 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+#def greet():
+ # print("Hello")
+  #print("how're you")
+  #print("Isn't the weather nice?")
+#greet()
+
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"how're you {name}")
+greet_with_name("Nam")
